beeaf49469754a6815e9436080f928d77cf89e26
beanstown106 <nbenis106@gmail.com>
2017-05-12 15:27:47 +0530

    Settings:Incall Vibration options [3/3]
    Signed-off-by: Anand Ramachandran <anand.nv1998@gmail.com>

--- src/com/android/settings/aim/InCallSettings.java
+++ src/com/android/settings/aim/InCallSettings.java
@@ -2,45 +2,46 @@
      Licensed under the Apache License, Version 2.0 (the "License");
      you may not use this file except in compliance with the License.
      You may obtain a copy of the License at
-          http://www.apache.org/licenses/LICENSE2.0
+
+          http://www.apache.org/licenses/LICENSE-2.0
+
      Unless required by applicable law or agreed to in writing, software
      distributed under the License is distributed on an "AS IS" BASIS,
      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
      See the License for the specific language governing permissions and
      limitations under the License.
 */
-package com.aim.freedomhub.tabs;
+package com.android.settings.aim;
+
+import android.os.Bundle;
+import android.support.v7.preference.ListPreference;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v7.preference.PreferenceScreen;
 
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 
-import android.os.Bundle;
 import com.android.settings.R;
-
-import android.support.v7.preference.PreferenceCategory;
-import android.support.v7.preference.PreferenceScreen;
 import com.android.settings.SettingsPreferenceFragment;
-
 import com.android.settings.Utils;
 
-import android.telephony.TelephonyManager;
-public class Misc extends SettingsPreferenceFragment {
+public class InCallSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
 
-private static final String AIM_INCALL = "aim_incall";
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.AIM;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        addPreferencesFromResource(R.xml.miscs);
-        PreferenceScreen prefScreen = getPreferenceScreen();
-        PreferenceCategory incallVibCategory = (PreferenceCategory) findPreference(AIM_INCALL);
-        if (!Utils.isVoiceCapable(getActivity())) {
-            prefScreen.removePreference(incallVibCategory);
-        }
+        addPreferencesFromResource(R.xml.aim_incall);
     }
 
-    @Override
-    protected int getMetricsCategory() {
-        return MetricsEvent.AMIFY;
+    public boolean onPreferenceChange(Preference preference, Object objValue) 		{
+        return true;
     }
 }
