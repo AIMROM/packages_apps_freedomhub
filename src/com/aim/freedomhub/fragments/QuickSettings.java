ViperOS/android_packages_apps_Venom

Custom small QS tiles [2/2]

src/com/viper/venom/fragments/QuickSettings.java
@@ -1,0 +1,64 @@
+package com.viper.venom.fragments;
+
+import android.os.Bundle;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.view.View;
+
+import com.android.settings.R;
+import com.viper.venom.preference.CustomSeekBarPreference;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+public class QuickSettings extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
+
+    private static final String PREF_SYSUI_QQS_COUNT = "sysui_qqs_count_key";
+
+    private ListPreference mSysuiQqsCount;
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.VENOM;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.quicksettings_settings);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        mSysuiQqsCount = (ListPreference) findPreference(PREF_SYSUI_QQS_COUNT);
+        int SysuiQqsCount = Settings.Secure.getInt(resolver,
+               Settings.Secure.QQS_COUNT, 5);
+        mSysuiQqsCount.setValue(Integer.toString(SysuiQqsCount));
+        mSysuiQqsCount.setSummary(mSysuiQqsCount.getEntry());
+        mSysuiQqsCount.setOnPreferenceChangeListener(this);
+     }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mSysuiQqsCount) {
+            String SysuiQqsCount = (String) newValue;
+            int SysuiQqsCountValue = Integer.parseInt(SysuiQqsCount);
+            Settings.Secure.putInt(resolver,
+                   Settings.Secure.QQS_COUNT, SysuiQqsCountValue);
+            int SysuiQqsCountIndex = mSysuiQqsCount.findIndexOfValue(SysuiQqsCount);
+            mSysuiQqsCount.setSummary(mSysuiQqsCount.getEntries()[SysuiQqsCountIndex]);
+            return true;
+        }
+        return false;
+    }
+}

AIMROM/packages_apps_freedomhub

Signed-off-by: Anand Ramachandran <anand.nv1998@gmail.com>
Conflicts:
	res/xml/aim_statusbar_tab.xml

src/com/aim/freedomhub/fragments/QuickSettings.java
@@ -1,0 +1,64 @@
+package com.aim.freedomhub.fragments;
+
+import android.os.Bundle;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.view.View;
+
+import com.android.settings.R;
+import com.android.settings.aim.Preference.CustomSeekBarPreference;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+public class QuickSettings extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
+
+    private static final String PREF_SYSUI_QQS_COUNT = "sysui_qqs_count_key";
+
+    private ListPreference mSysuiQqsCount;
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.AIM;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.quicksettings_settings);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        mSysuiQqsCount = (ListPreference) findPreference(PREF_SYSUI_QQS_COUNT);
+        int SysuiQqsCount = Settings.Secure.getInt(resolver,
+               Settings.Secure.QQS_COUNT, 5);
+        mSysuiQqsCount.setValue(Integer.toString(SysuiQqsCount));
+        mSysuiQqsCount.setSummary(mSysuiQqsCount.getEntry());
+        mSysuiQqsCount.setOnPreferenceChangeListener(this);
+     }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mSysuiQqsCount) {
+            String SysuiQqsCount = (String) newValue;
+            int SysuiQqsCountValue = Integer.parseInt(SysuiQqsCount);
+            Settings.Secure.putInt(resolver,
+                   Settings.Secure.QQS_COUNT, SysuiQqsCountValue);
+            int SysuiQqsCountIndex = mSysuiQqsCount.findIndexOfValue(SysuiQqsCount);
+            mSysuiQqsCount.setSummary(mSysuiQqsCount.getEntries()[SysuiQqsCountIndex]);
+            return true;
+        }
+        return false;
+    }
+}