95421f25c827c60fc8321edd95a2593da6f3e640
Anand Ramachandran <anand.nv1998@gmail.com>
2017-07-09 16:14:11 +0530

    fix

--- src/com/aim/freedomhub/InCallSettings.java
+++ src/com/aim/freedomhub/InCallSettings.java
@@ -11,7 +11,7 @@
      See the License for the specific language governing permissions and
      limitations under the License.
 */
-package com.android.settings.aim;
+package com.aim.freedomhub;
 
 import android.os.Bundle;
 import android.support.v7.preference.ListPreference;
