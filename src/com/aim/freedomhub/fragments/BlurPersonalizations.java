shivatejapeddi/packages_apps_CustomSquash

[SQUASHED] Implement XOSP Blur personalization options (2/2) All credits for original work goes to serjar (Author of the very famous Xposed module: Blurred System UI)
Credits goes also to: Aaahh  for the initial implementation of it in MarshROM and continued by me after in XOSP

Original repository of the Xposed Module: https://github.com/serajr/Blurred-System-UI

Available options:

- Blurred expanded StatusBar
- Blurred StausBar header (Deprecated on N)
- Translucent Notifications
- Blurred QS in Notification Header expanded
- Ability to change Blur Radius and Blur scale

Squash all my previous commits from Marshmallow and adapt everything to work on N:

nilac8991/xosp_packages_apps_Settings@fa72098
nilac8991/xosp_packages_apps_Settings@06fdcc8
nilac8991/xosp_packages_apps_Settings@2f786e1

//TO-DO

- Adapt and finish Recents Blur code for Nougat
- Make Translucent Notifications work (Because right now it's completely wrong, so it will be cut off from Settings)

Signed-off-by: Calin <nilac8991@gmail.com>

Citrus Edits:

Place it inside Ui Tab instead of Seperated tab.

Add BlurPersonalization fragment from: XOSP-Project/android_packages_apps_Settings@7095caf

Commit Also contains:
XOSP-Project/android_packages_apps_Settings@0c78104
XOSP-Project/android_packages_apps_Settings@0c78104

Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

src/com/citrus/settings/fragments/BlurPersonalizations.java
@@ -1,0 +1,153 @@
+/*
+ * Copyright (C) 2016 The Xperia Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.citrus.settings.fragments;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
+import android.telephony.TelephonyManager;
+import android.text.format.DateFormat;
+import android.view.View;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.citrus.settings.preference.CustomSeekBarPreference;
+
+public class BlurPersonalizations extends SettingsPreferenceFragment
+        implements OnPreferenceChangeListener {
+
+//Switch Preferences
+    private SwitchPreference mExpand;
+    private SwitchPreference mNotiTrans;
+    private SwitchPreference mQuickSett;
+    
+    //Transluency,Radius and Scale
+    private CustomSeekBarPreference mScale;
+    private CustomSeekBarPreference mRadius;
+    private CustomSeekBarPreference mQuickSettPerc;
+    private CustomSeekBarPreference mNotSettPerc;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.blur_cat);
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        mExpand = (SwitchPreference) prefSet.findPreference("blurred_status_bar_expanded_enabled_pref");
+        mExpand.setChecked((Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.STATUS_BAR_EXPANDED_ENABLED_PREFERENCE_KEY, 0) == 1));
+
+        mScale = (CustomSeekBarPreference) findPreference("statusbar_blur_scale");
+        mScale.setValue(Settings.System.getInt(resolver, Settings.System.BLUR_SCALE_PREFERENCE_KEY, 10));
+        mScale.setOnPreferenceChangeListener(this);
+
+        mRadius = (CustomSeekBarPreference) findPreference("statusbar_blur_radius");
+        mRadius.setValue(Settings.System.getInt(resolver, Settings.System.BLUR_RADIUS_PREFERENCE_KEY, 5));
+        mRadius.setOnPreferenceChangeListener(this);
+
+        /* mNotiTrans = (SwitchPreference) prefSet.findPreference("translucent_notifications_pref");
+        mNotiTrans.setChecked((Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.TRANSLUCENT_NOTIFICATIONS_PREFERENCE_KEY, 0) == 1)); */
+
+        mQuickSett = (SwitchPreference) prefSet.findPreference("translucent_quick_settings_pref");
+        mQuickSett.setChecked((Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.TRANSLUCENT_QUICK_SETTINGS_PREFERENCE_KEY, 0) == 1));
+
+        mQuickSettPerc = (CustomSeekBarPreference) findPreference("quick_settings_transluency");
+        mQuickSettPerc.setValue(Settings.System.getInt(resolver, Settings.System.TRANSLUCENT_QUICK_SETTINGS_PRECENTAGE_PREFERENCE_KEY, 60));
+        mQuickSettPerc.setOnPreferenceChangeListener(this);
+
+       /* mNotSettPerc = (CustomSeekBarPreference) findPreference("notifications_transluency");
+        mNotSettPerc.setValue(Settings.System.getInt(resolver, Settings.System.TRANSLUCENT_NOTIFICATIONS_PRECENTAGE_PREFERENCE_KEY, 60));
+        mNotSettPerc.setOnPreferenceChangeListener(this); */
+
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.CUSTOM_SQUASH;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mScale) {
+            int value = ((Integer)newValue).intValue();
+            Settings.System.putInt(
+                resolver, Settings.System.BLUR_SCALE_PREFERENCE_KEY, value);
+            return true;
+        } else if (preference == mRadius) {
+            int value = ((Integer)newValue).intValue();
+            Settings.System.putInt(
+                resolver, Settings.System.BLUR_RADIUS_PREFERENCE_KEY, value);
+            return true;
+        } else if (preference == mQuickSettPerc) {
+            int value = ((Integer)newValue).intValue();
+            Settings.System.putInt(
+                resolver, Settings.System.TRANSLUCENT_QUICK_SETTINGS_PRECENTAGE_PREFERENCE_KEY, value);
+            return true;
+      /*  } else if (preference == mNotSettPerc) {
+            int value = ((Integer)newValue).intValue();
+            Settings.System.putInt(
+                resolver, Settings.System.TRANSLUCENT_NOTIFICATIONS_PRECENTAGE_PREFERENCE_KEY, value);
+            return true; */
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        if  (preference == mExpand) {
+            boolean enabled = ((SwitchPreference)preference).isChecked();
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.STATUS_BAR_EXPANDED_ENABLED_PREFERENCE_KEY, enabled ? 1:0);
+        } else if (preference == mNotiTrans) {
+            boolean enabled = ((SwitchPreference)preference).isChecked();
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.TRANSLUCENT_NOTIFICATIONS_PREFERENCE_KEY, enabled ? 1:0);     
+        } else if (preference == mQuickSett) {
+            boolean enabled = ((SwitchPreference)preference).isChecked();
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.TRANSLUCENT_QUICK_SETTINGS_PREFERENCE_KEY, enabled ? 1:0); 
+        }
+        return super.onPreferenceTreeClick(preference);
+    }
+}

AIMROM/packages_apps_freedomhub

Signed-off-by: Anand Ramachandran <anand.nv1998@gmail.com>

src/com/aim/freedomhub/fragments/BlurPersonalizations.java
@@ -1,0 +1,153 @@
+/*
+ * Copyright (C) 2016 The Xperia Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aim.freedomhub.fragments;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
+import android.telephony.TelephonyManager;
+import android.text.format.DateFormat;
+import android.view.View;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.aim.freedomhub.preferences.CustomSeekBarPreference;
+
+public class BlurPersonalizations extends SettingsPreferenceFragment
+        implements OnPreferenceChangeListener {
+
+//Switch Preferences
+    private SwitchPreference mExpand;
+    private SwitchPreference mNotiTrans;
+    private SwitchPreference mQuickSett;
+
+    //Transluency,Radius and Scale
+    private CustomSeekBarPreference mScale;
+    private CustomSeekBarPreference mRadius;
+    private CustomSeekBarPreference mQuickSettPerc;
+    private CustomSeekBarPreference mNotSettPerc;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.blur_cat);
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        mExpand = (SwitchPreference) prefSet.findPreference("blurred_status_bar_expanded_enabled_pref");
+        mExpand.setChecked((Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.STATUS_BAR_EXPANDED_ENABLED_PREFERENCE_KEY, 0) == 1));
+
+        mScale = (CustomSeekBarPreference) findPreference("statusbar_blur_scale");
+        mScale.setValue(Settings.System.getInt(resolver, Settings.System.BLUR_SCALE_PREFERENCE_KEY, 10));
+        mScale.setOnPreferenceChangeListener(this);
+
+        mRadius = (CustomSeekBarPreference) findPreference("statusbar_blur_radius");
+        mRadius.setValue(Settings.System.getInt(resolver, Settings.System.BLUR_RADIUS_PREFERENCE_KEY, 5));
+        mRadius.setOnPreferenceChangeListener(this);
+
+        /* mNotiTrans = (SwitchPreference) prefSet.findPreference("translucent_notifications_pref");
+        mNotiTrans.setChecked((Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.TRANSLUCENT_NOTIFICATIONS_PREFERENCE_KEY, 0) == 1)); */
+
+        mQuickSett = (SwitchPreference) prefSet.findPreference("translucent_quick_settings_pref");
+        mQuickSett.setChecked((Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.TRANSLUCENT_QUICK_SETTINGS_PREFERENCE_KEY, 0) == 1));
+
+        mQuickSettPerc = (CustomSeekBarPreference) findPreference("quick_settings_transluency");
+        mQuickSettPerc.setValue(Settings.System.getInt(resolver, Settings.System.TRANSLUCENT_QUICK_SETTINGS_PRECENTAGE_PREFERENCE_KEY, 60));
+        mQuickSettPerc.setOnPreferenceChangeListener(this);
+
+       /* mNotSettPerc = (CustomSeekBarPreference) findPreference("notifications_transluency");
+        mNotSettPerc.setValue(Settings.System.getInt(resolver, Settings.System.TRANSLUCENT_NOTIFICATIONS_PRECENTAGE_PREFERENCE_KEY, 60));
+        mNotSettPerc.setOnPreferenceChangeListener(this); */
+
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.AIM;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mScale) {
+            int value = ((Integer)newValue).intValue();
+            Settings.System.putInt(
+                resolver, Settings.System.BLUR_SCALE_PREFERENCE_KEY, value);
+            return true;
+        } else if (preference == mRadius) {
+            int value = ((Integer)newValue).intValue();
+            Settings.System.putInt(
+                resolver, Settings.System.BLUR_RADIUS_PREFERENCE_KEY, value);
+            return true;
+        } else if (preference == mQuickSettPerc) {
+            int value = ((Integer)newValue).intValue();
+            Settings.System.putInt(
+                resolver, Settings.System.TRANSLUCENT_QUICK_SETTINGS_PRECENTAGE_PREFERENCE_KEY, value);
+            return true;
+      /*  } else if (preference == mNotSettPerc) {
+            int value = ((Integer)newValue).intValue();
+            Settings.System.putInt(
+                resolver, Settings.System.TRANSLUCENT_NOTIFICATIONS_PRECENTAGE_PREFERENCE_KEY, value);
+            return true; */
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        if  (preference == mExpand) {
+            boolean enabled = ((SwitchPreference)preference).isChecked();
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.STATUS_BAR_EXPANDED_ENABLED_PREFERENCE_KEY, enabled ? 1:0);
+        } else if (preference == mNotiTrans) {
+            boolean enabled = ((SwitchPreference)preference).isChecked();
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.TRANSLUCENT_NOTIFICATIONS_PREFERENCE_KEY, enabled ? 1:0);
+        } else if (preference == mQuickSett) {
+            boolean enabled = ((SwitchPreference)preference).isChecked();
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.TRANSLUCENT_QUICK_SETTINGS_PREFERENCE_KEY, enabled ? 1:0);
+        }
+        return super.onPreferenceTreeClick(preference);
+    }
+}