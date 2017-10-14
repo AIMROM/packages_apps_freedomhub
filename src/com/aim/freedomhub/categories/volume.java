fffa1e0c6ea031b929e6ba87ca03e9e62d3a500d
Anand Ramachandran <anand.nv1998@gmail.com>
2017-10-14 20:05:18 +0530

    Nuke unwanted feature and categorize

--- src/com/aim/freedomhub/categories/volume.java
+++ src/com/aim/freedomhub/categories/volume.java
@@ -1,90 +1,58 @@
-/*
- * Copyright (C) 2017 AIM ROM
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+/**
+    Copyright 2017 AIM ROM
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+        http://www.apache.org/licenses/LICENSE-2.0
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
 
 package com.aim.freedomhub.categories;
 
-import android.app.ActivityManagerNative;
-import android.content.Context;
+import com.android.internal.logging.nano.MetricsProto;
 import android.content.ContentResolver;
-import android.content.pm.PackageManager.NameNotFoundException;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.UserHandle;
-import android.os.RemoteException;
-import android.os.ServiceManager;
-import android.support.v14.preference.SwitchPreference;
-import android.support.v7.preference.Preference;
 import android.support.v7.preference.ListPreference;
-import android.support.v7.preference.PreferenceCategory;
-import android.support.v7.preference.PreferenceScreen;
-import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v7.preference.Preference;
+import android.preference.SwitchPreference;
 import android.provider.Settings;
-import android.util.Log;
-import android.view.WindowManagerGlobal;
-import android.view.IWindowManager;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import java.util.Locale;
-import android.text.TextUtils;
-import android.view.View;
+import android.provider.Settings.SettingNotFoundException;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.internal.logging.nano.MetricsProto;
-import com.android.settings.Utils;
 
-public class volume extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+public class volume extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
 
- private static final String HEADSET_CONNECT_PLAYER = "headset_connect_player";
- private static final String RINGTONE_FOCUS_MODE = "ringtone_focus_mode";
+    private static final String HEADSET_CONNECT_PLAYER = "headset_connect_player";
 
- private ListPreference mLaunchPlayerHeadsetConnection;
- private ListPreference mHeadsetRingtoneFocus;
+    private ListPreference mLaunchPlayerHeadsetConnection;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
 
         addPreferencesFromResource(R.xml.sound_settings);
 
-        final ContentResolver resolver = getActivity().getContentResolver();
-        final PreferenceScreen prefSet = getPreferenceScreen();
-
-		mLaunchPlayerHeadsetConnection = (ListPreference) findPreference(HEADSET_CONNECT_PLAYER);
+        ContentResolver resolver = getActivity().getContentResolver();
+  		  
+        mLaunchPlayerHeadsetConnection = (ListPreference) findPreference(HEADSET_CONNECT_PLAYER);
         int mLaunchPlayerHeadsetConnectionValue = Settings.System.getIntForUser(resolver,
                 Settings.System.HEADSET_CONNECT_PLAYER, 4, UserHandle.USER_CURRENT);
         mLaunchPlayerHeadsetConnection.setValue(Integer.toString(mLaunchPlayerHeadsetConnectionValue));
         mLaunchPlayerHeadsetConnection.setSummary(mLaunchPlayerHeadsetConnection.getEntry());
         mLaunchPlayerHeadsetConnection.setOnPreferenceChangeListener(this);
 
-        mHeadsetRingtoneFocus = (ListPreference) findPreference(RINGTONE_FOCUS_MODE);
-        int mHeadsetRingtoneFocusValue = Settings.Global.getInt(resolver,
-                Settings.Global.RINGTONE_FOCUS_MODE, 0);
-        mHeadsetRingtoneFocus.setValue(Integer.toString(mHeadsetRingtoneFocusValue));
-        mHeadsetRingtoneFocus.setSummary(mHeadsetRingtoneFocus.getEntry());
-        mHeadsetRingtoneFocus.setOnPreferenceChangeListener(this);
     }
 
     @Override
-    public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.AIM;
-    }
-
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+
         ContentResolver resolver = getActivity().getContentResolver();
         if (preference == mLaunchPlayerHeadsetConnection) {
             int mLaunchPlayerHeadsetConnectionValue = Integer.valueOf((String) newValue);
@@ -94,15 +62,14 @@ public class volume extends SettingsPreferenceFragment implements OnPreferenceCh
             Settings.System.putIntForUser(resolver, Settings.System.HEADSET_CONNECT_PLAYER,
                     mLaunchPlayerHeadsetConnectionValue, UserHandle.USER_CURRENT);
             return true;
-        } else if (preference == mHeadsetRingtoneFocus) {
-            int mHeadsetRingtoneFocusValue = Integer.valueOf((String) newValue);
-            int index = mHeadsetRingtoneFocus.findIndexOfValue((String) newValue);
-            mHeadsetRingtoneFocus.setSummary(
-                    mHeadsetRingtoneFocus.getEntries()[index]);
-            Settings.Global.putInt(resolver, Settings.Global.RINGTONE_FOCUS_MODE,
-                    mHeadsetRingtoneFocusValue);
-            return true;
         }
+
         return false;
     }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.AIM;
+    }
 }
+
