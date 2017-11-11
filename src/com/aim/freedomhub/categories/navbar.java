64558be3c734c753c195700f3a9005f2ed6056a4
ezio84 <brabus84@gmail.com>
2017-11-12 03:57:13 +0530

    Add NavBar toggle [2/2]
    Change-Id: I0fa7a44925bd3799d8cfd3562b60541f55e55873
    Signed-off-by: Anand Ramachandran <anand.nv1998@gmail.com>

--- src/com/aim/freedomhub/categories/navbar.java
+++ src/com/aim/freedomhub/categories/navbar.java
@@ -1,50 +1,96 @@
 /*
-  * Copyright (C) 2017 AIMROM
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *      http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
- package com.aim.freedomhub.categories;
- 
- import android.content.ContentResolver;
- import android.content.res.Resources;
- import android.os.Bundle;
- import android.support.v7.preference.ListPreference;
- import android.support.v7.preference.Preference;
- import android.support.v7.preference.PreferenceScreen;
- import android.provider.Settings;
- 
- import com.android.internal.logging.nano.MetricsProto;
- 
- import java.io.FileNotFoundException;
- import com.android.settings.R;
- import com.android.settings.SettingsPreferenceFragment;
- 
- public class navbar extends SettingsPreferenceFragment implements
-         Preference.OnPreferenceChangeListener {
- 
-     @Override
-     public void onCreate(Bundle savedInstanceState) {
-         super.onCreate(savedInstanceState);
-         addPreferencesFromResource(R.xml.navbar_settings);
- 
-     }
- 
-     @Override
-     public int getMetricsCategory() {
-         return MetricsProto.MetricsEvent.AIM;
-     }
- 
-     public boolean onPreferenceChange(Preference preference, Object newValue) {
-         return false;
-     }
- }
+ * Copyright (C) 2017 AIMROM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aim.freedomhub.categories;
+
+import android.app.ActivityManagerNative;
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.WindowManagerGlobal;
+import android.view.IWindowManager;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import java.util.Locale;
+import android.text.TextUtils;
+import android.view.View;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.Utils;
+
+public class navbar extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+
+    private SwitchPreference mNavbarToggle;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.navbar_settings);
+
+        final ContentResolver resolver = getActivity().getContentResolver();
+        final PreferenceScreen prefSet = getPreferenceScreen();
+
+        mNavbarToggle = (SwitchPreference) findPreference("navigation_bar_enabled");
+        boolean enabled = Settings.Secure.getIntForUser(
+                resolver, Settings.Secure.NAVIGATION_BAR_ENABLED,
+                getActivity().getResources().getBoolean(
+                com.android.internal.R.bool.config_showNavigationBar) ? 1 : 0,
+                UserHandle.USER_CURRENT) == 1;
+        mNavbarToggle.setChecked(enabled);
+        mNavbarToggle.setOnPreferenceChangeListener(this);
+
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.AIM;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mNavbarToggle) {
+            boolean value = (Boolean) newValue;
+            Settings.Secure.putIntForUser(getActivity().getContentResolver(),
+                    Settings.Secure.NAVIGATION_BAR_ENABLED, value ? 1 : 0,
+                    UserHandle.USER_CURRENT);
+            mNavbarToggle.setChecked(value);
+            return true;
+        }
+        return false;
+    }
+}
